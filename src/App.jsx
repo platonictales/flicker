e9fa353a213462c2e11f284c9b9633e9ab71ddc9
smoothfrictionflow <smoothfrictionflow@gmail.com>
2025-06-02 09:07:30 +0530

    feat: openingscreen with new and open buttons
    Todo:
    1. not min and max buttons on window only close button
    2. New opens a new editor in maximum size

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import WritingCanvas from "./components/WritingCanvas";
+import OpeningScreen from "./components/OpeningScreen";
+import React, { useState } from "react";
 
 function App() {
+  const [showCanvas, setShowCanvas] = useState(false);
+
+  const handleNew = () => {
+    setShowCanvas(true);
+  };
+  const handleOpen = () => {
+    alert("Open functionality is not implemented yet!"); // Placeholder for "Open"
+  };
   return (
     <main className="container">
-      <WritingCanvas />
+      {showCanvas ? (
+        <WritingCanvas />
+      ) : (
+        <OpeningScreen onNew={handleNew} onOpen={handleOpen} />
+      )}
     </main>
   );
 }
